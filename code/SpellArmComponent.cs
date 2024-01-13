fe3114e9904cc05f0eae70cd095d90966a1bf3a0
Retroeer <retroneer128@gmail.com>
2024-01-13 14:23:18 -0500

    Cooldowns for spells
    pretty basic, once more spells are in i imagine this will probably need a second look

--- code/SpellArmComponent.cs
+++ code/SpellArmComponent.cs
@@ -1,25 +1,36 @@
 using Sandbox;
 
+
+
 public sealed class SpellArmComponent : Component
 {
 	[Property] SkinnedModelRenderer ViewModel { get; set; }
 	protected override void OnFixedUpdate()
 	{
-		base.OnFixedUpdate();
 		if ( Input.Pressed( "attack2" ) )
+
+			
 			CastSpells();
 			
 	}
 
 	void CastSpells()
 	{
-		ViewModel.Set( "b_fire", true );
+		
 		var spell = Components.Get<ISpell>();
+		
+		if (spell.canCast())
+		{
 		spell.CastSpell();
+		ViewModel.Set( "b_fire", true );
+		}
+		
+
 	}
 }
 
 public interface ISpell
 {
 	public void CastSpell();
+	public bool canCast();
 }
